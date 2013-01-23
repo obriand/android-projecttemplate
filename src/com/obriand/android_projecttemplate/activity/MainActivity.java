65b5446e2d8fc3a746a2d88ea1c861927bba4db0
Olivier Briand <olivier.briand@orange.com>
2013-01-23 14:50:02 +0100

    Add the code in the main activity

--- src/com/obriand/android_projecttemplate/activity/MainActivity.java
+++ src/com/obriand/android_projecttemplate/activity/MainActivity.java
@@ -3,17 +3,40 @@ package com.obriand.android_projecttemplate.activity;
 import com.obriand.android_projecttemplate.R;
 import com.obriand.android_projecttemplate.R.layout;
 import com.obriand.android_projecttemplate.R.menu;
+import com.obriand.android_projecttemplate.config.ConfigurationManager;
+import com.obriand.android_projecttemplate.dependency.DependencyResolver;
+import com.obriand.android_projecttemplate.dependency.DependencyResolverImpl;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+/**
+ * Main activity displayed to the user once he launches the application. 
+ */
 public class MainActivity extends Activity {
+	
+	private static final String LOG_TAG = "MainActivity";
+	
+	private ConfigurationManager mConfiguration;
+	
+	/**
+	 * ProjectData object used to access remote content and to authenticate against ProjectData
+	 * Internet API when the user selects the menu item.
+	 */
+//	private ProjectData mProjectData;
+
+	private DependencyResolver mResolver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mResolver = DependencyResolverImpl.getInstance();
+//		mDailymotion = mResolver.getDailymotion();
+		mConfiguration = mResolver.getConfiguration();
+		
 	}
 
 	@Override
